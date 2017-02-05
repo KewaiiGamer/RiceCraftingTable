6ec0fed7165c1ff7e086619d1b78d96b90a08db4
KewaiiGamer <miguelviegas1999@gmail.com>
2017-02-05 09:28:30 +0000

    Update version values to 1.0.1

--- src/main/java/kewaiigamer/rice/Rice.java
+++ src/main/java/kewaiigamer/rice/Rice.java
@@ -13,7 +13,7 @@ public class Rice {
 
     public static final String MODID = "rice";
     public static final String NAME = "Rice";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.1";
     public static final String COMMON_PROXY_CLASS = "kewaiigamer.rice.CommonProxy";
     public static final String CLIENT_PROXY_CLASS = "kewaiigamer.rice.ClientProxy";
     @Instance(MODID)
